9329d97f69f825a339b78b88912e9736d20ed709
Taichi Ichijo <s23C1015UT@s.chibakoudai.jp>
2024-11-25 20:40:33 +0900

    kadai

--- testaverage.bash
+++ testaverage.bash
@@ -10,14 +10,12 @@ ng () {
 res=0
 
 ### 正常動作 ###
-# 入力が 1, 2, 3, 4, 5 の場合、平均値は 3.0
 out=$(seq 5 | ./average.py)
 if ! echo "$out" | grep -q "平均値: 3.000000"; then
     ng "$LINENO"
 fi
 
 ### 異常動作 ###
-# 無効な入力がある場合（例: "あ"）
 out=$(echo あ | ./average.py 2>&1)
 if ! echo "$out" | grep -q "無効な入力がありました: あ"; then
     ng "$LINENO"
